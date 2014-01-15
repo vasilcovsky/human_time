eda2910b43cc7532a4075ac3e1af1482971b56ea
Igor Vasilcovsky <vasilcovsky@gmail.com>
2014-01-15 01:14:43 -0500

    Improved code style

--- filters.py
+++ filters.py
@@ -1,35 +1,16 @@
 """
 Filters are intended to remove datetimes not meeting our criteria or to
 alter datetimes (currently only to apply a time of day to a date).
-
-The id impletmentation would be _filter_allow
 """
 
-import calendar
 import re
-from functools import partial, wraps
-from datetime import datetime, timedelta
+import calendar
 import consts
 
-day_indexes = dict(
-    monday    = [0],
-    tuesday   = [1],
-    wednesday = [2],
-    thursday  = [3],
-    friday    = [4],
-    saturday  = [5],
-    sunday    = [6],
-    weekday   = [0,1,2,3,4],
-    weekend   = [5,6],
-    day       = [0,1,2,3,4,5,6],
-)
-
-time_indexes = dict(
-    noon    = (12,0),
-    midday  = (12,0),
-    morning = (8,0),
-)
-
+from functools import partial, wraps
+from datetime import datetime, timedelta
+from consts import DAY_INDEXES, TIME_INDEXES, SELECTOR_INDEXES
+from consts import TimePattern
 
 def generic_filter(filter_func):
     """Decorates filter function to value generator.
@@ -58,15 +39,12 @@ def generic_filter(filter_func):
     def filter_(regex_result):
         def f(gen):
             for v in gen:
-              if(filter_func(regex_result, v)):
-                yield v
+                if filter_func(regex_result, v):
+                    yield v
         return f
+
     return filter_
 
-
-@generic_filter
-def _filter_allow(regex_result, item):
-    pass
 
 
 def _filter_everyother(regex_result):
@@ -82,21 +60,12 @@ def _filter_everyother(regex_result):
 @generic_filter
 def _filter_weekday(regex_result, item):
     the_day = regex_result.groupdict()['principle']
-    acceptable_days = day_indexes[the_day]
-    return item.weekday() in acceptable_days
+    return item.weekday() in DAY_INDEXES[the_day]
 
-
-selector_indexes = dict(
-    first  = 0,
-    second = 1,
-    third  = 2,
-    fourth = 3,
-    last   = -1,
-)
 
 def _get_xs_in_month(x, year, month):
     """Used to get all Xs from a month where X is something like Tuesday"""
-    day_index = day_indexes[x][0]
+    day_index = DAY_INDEXES[x][0]
     c = calendar.monthcalendar(year, month)
     results = [week[day_index] for week in c if week[day_index]]
 
@@ -108,8 +77,8 @@ def _filter_identifier_in_month(regex_result, item):
     selector = regex_result.groupdict()['selector']
     the_day = regex_result.groupdict()['principle']
     
-    acceptable_days = day_indexes[the_day]
-    selector_index = selector_indexes[selector]
+    acceptable_days = DAY_INDEXES[the_day]
+    selector_index = SELECTOR_INDEXES[selector]
     
     if item.weekday() in acceptable_days:
         xs_in_month = _get_xs_in_month(the_day, item.year, item.month)
@@ -127,10 +96,10 @@ def _filter_identifier_in_month_after(regex_result, item):
 
     #calculate the 'after' date of the month
     xs_in_month = _get_xs_in_month(the_day2, item.year, item.month)
-    after = datetime(item.year, item.month, xs_in_month[selector_indexes[selector2]])
+    after = datetime(item.year, item.month, xs_in_month[SELECTOR_INDEXES[selector2]])
 
-    acceptable_days = day_indexes[the_day]
-    selector_index = selector_indexes[selector]
+    acceptable_days = DAY_INDEXES[the_day]
+    selector_index = SELECTOR_INDEXES[selector]
     
     if item.weekday() in acceptable_days and after < item:
         xs_in_month = _get_xs_in_month(the_day, item.year, item.month)
@@ -156,34 +125,25 @@ def _filter_end_of_month(regex_result, item):
 """Application functions take a value and apply changes
 to it before yielding it on.
 """
-_compiled_12_hour_time = re.compile(consts.re_12_hour_time.replace("?:", ""))
-_compiled_12_hourmin_time = re.compile(consts.re_12_hourmin_time.replace("?:", ""))
-_compiled_24_hour_time = re.compile(consts.re_24_hour_time.replace("?:", ""))
-_compiled_time_names = re.compile(consts.re_time_names.replace("?:", ""))
-_compiled_this_time = re.compile(consts.re_this_time)
+#_compiled_12_hour_time = re.compile(Pattern.TIME_12H)
+_compiled_12_hourmin_time = re.compile(TimePattern.TIME_12H)
+_compiled_24_hour_time = re.compile(TimePattern.TIME_24H)
+_compiled_time_names = re.compile(TimePattern.TIME_TERM)
+_compiled_this_time = re.compile(TimePattern.TIME_CURRENT)
+
 def _apply_time(regex_result):
     def f(gen, hour, minute):
         for v in gen:
             yield datetime(v.year, v.month, v.day, hour, minute)
 
     the_time = regex_result.groupdict()['applicant']
-    
-    # First try it for 12 hour time
-    r = _compiled_12_hour_time.match(the_time)
-    if r:
-        hour, suffix = r.groups()
-        hour = int(hour)
-        if suffix == "pm":
-            hour += 12
-        
-        return partial(f, hour=hour, minute=0)
 
-    # 12 hour with minutes?
     r = _compiled_12_hourmin_time.match(the_time)
     if r:
-        hour, minute, suffix = r.groups()
-        hour = int(hour)
-        minute = int(minute)
+        groupdict = r.groupdict()
+        hour = int(groupdict.get("hour") or 0)
+        minute = int(groupdict.get("minute") or 0)
+        suffix = groupdict.get("suffix")
         if suffix == "pm":
             hour += 12
         
@@ -200,7 +160,7 @@ def _apply_time(regex_result):
     # Named time
     r = _compiled_time_names.match(the_time)
     if r:
-        hour, minute = time_indexes[r.groups()[0]]
+        hour, minute = TIME_INDEXES[r.groups()[0]]
         return partial(f, hour=hour, minute=minute)      
     
     # Relative time
@@ -212,6 +172,7 @@ def _apply_time(regex_result):
         return f
 
     raise Exception("Unable to find time applicant for '{}'".format(the_time))
+
 
 def _cut_time(regex_result):
     def f(gen):
